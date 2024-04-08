a9a8b7a307bc2e7bc5be23e26aaf2deedc9c577e
Burt <154017141+Burt8@users.noreply.github.com>
2024-04-07 21:57:51 -0400

    Make a loop that creates a div 256 times
    Create a for loop that passes 256 iterations
    The code inside the loop creates a div with the class box
    The box-class div is placed inside the container div

--- sketch.js
+++ sketch.js
@@ -1,2 +1,7 @@
 let container = document.querySelector(".container");
-const div = document.createElement("div");
+
+for (let i = 0; i < 256; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box")
+    container.appendChild(box)
+};
