23efd81cdd446d2c4e328723cc1c049df8a013cd
Burt <154017141+Burt8@users.noreply.github.com>
2024-04-07 23:16:12 -0400

    create variable numBoxes to hold number of boxes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let container = document.querySelector(".container");
+let numBoxes = 256;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < numBoxes; i++) {
     const box = document.createElement("div");
     box.classList.add("box")
     container.appendChild(box)
