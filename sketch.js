f2a76d9e4fef9a65d7e138fb824d024d70dfdf13
Burt <154017141+Burt8@users.noreply.github.com>
2024-04-11 11:12:42 -0400

    add x and y (axis) declarations, changes grid size

--- sketch.js
+++ sketch.js
@@ -20,9 +20,15 @@ function addPaint() {
 }
 
 size.onclick = () => {
+    let y = prompt("How tall do you want the canvas to be? (Y-Axis)")
+    let x = prompt("How wide would you like your canvas to be? (X-Axis)")
+    numBoxes = x * y
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild)
     }
     gridGen()
     addPaint()
 }
+
+gridGen()
+addPaint()
