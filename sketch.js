8a5f2c683ca395f9fc25dfce85ecf1bb281d87d4
Burt <154017141+Burt8@users.noreply.github.com>
2024-04-23 17:31:23 -0400

    Add resize() that distributes equal box space

--- sketch.js
+++ sketch.js
@@ -1,14 +1,30 @@
 const canvas = document.querySelector(".canvas");
 let size = document.querySelector(".size");
 let numBoxes = 256;
+let pixels = 0
 
+function basisCalc() {
+    let boxSize = 600/Math.sqrt(numBoxes);
+    pixels = ((boxSize - 3)/6)
+}
+function resize() {
+    basisCalc()
+    let style = document.createElement("style");
+    style.innerHTML = ".box {flex-basis: " + pixels + "%}"
+    if (document.querySelector("style") === true) {
+        document.removeChild(style)
+    } else {    
+        document.head.appendChild(style)
+    }
+}
 function gridGen() {
     for (let i = 0; i < numBoxes; i++) {
         const box = document.createElement("div");
         box.classList.add("box")
         canvas.appendChild(box)
-    };
-}
+    }
+ };
+
 
 function addPaint() {
     let boxes = document.querySelectorAll(".box");
@@ -20,15 +36,22 @@ function addPaint() {
 }
 
 size.onclick = () => {
-    let y = prompt("How tall do you want the canvas to be? (Y-Axis)")
-    let x = prompt("How wide would you like your canvas to be? (X-Axis)")
-    numBoxes = x * y
+    let y = prompt("How many squares would you like per grid?")
+    if (y > 100) {
+        alert("Please choose a number below 101")
+        return null
+    } else if (y === NaN) {
+        alert("Please enter a NUMBER")
+    }
+    numBoxes = y * y
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild)
     }
     gridGen()
     addPaint()
+    basisCalc()
 }
 
 gridGen()
 addPaint()
+resize()
